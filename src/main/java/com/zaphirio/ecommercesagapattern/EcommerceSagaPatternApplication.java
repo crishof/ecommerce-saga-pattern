bf08f242e6d3a46f50773768c6ed4a9632e7f54f
crishof <cristianhof@gmail.com>
2026-07-12 07:10:14 -0300

    first commit

---
+++ src/main/java/com/zaphirio/ecommercesagapattern/EcommerceSagaPatternApplication.java
@@ -0,0 +1,13 @@
+package com.zaphirio.ecommercesagapattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSagaPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceSagaPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaphirio/ecommercesagapattern/EcommerceSagaPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaphirio.ecommercesagapattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSagaPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
